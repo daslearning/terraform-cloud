ab7455fdf7b2c4b6560f40ec31630af7b4f8f888
Somnath Das <somnath.das@daslearning.in>
2023-09-10 12:54:03 +0530

    upgrade the module

--- gcs.tf
+++ gcs.tf
@@ -1,19 +1,24 @@
 # Deploy gcs bucket using terraform cloud
 
+# Create bucket using module
+module "dl-test" {
+  source  = "app.terraform.io/daslearning-new/dl-test/google"
+  version = "0.0.4"
+  
+  bucket_name = "daslearning-m-bt"
+  location    = "asia-south1"
+  project     = "daslearning"
+}
+
+output "bucket_id" {
+  value = module.dl-test.bucket_id
+}
+
+/*
 provider "google" {
   project      = "daslearning"
 }
 
-# Create bucket using module
-module "dl-test" {
-  source  = "app.terraform.io/daslearning-new/dl-test/google"
-  version = "0.0.3"
-  
-  bucket_name = "daslearning-m-bt"
-  location    = "asia-south1"
-}
-
-/*
 resource "google_storage_bucket" "bucket2" {
   name     = "daslearning-tf-cld2"
   location = "ASIA-SOUTH1"
