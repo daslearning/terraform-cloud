8fdcc4f9d893fda4eb377a22a29c77df25318461
Somnath Das <somnath.das@daslearning.in>
2023-09-10 12:39:40 +0530

    use tf cloud module

--- gcs.tf
+++ gcs.tf
@@ -2,17 +2,18 @@
 
 provider "google" {
   project      = "daslearning"
-  region       = "us-central1"
-  zone         = "us-central1-b"
+}
+
+# Create bucket using module
+module "dl-test" {
+  source  = "app.terraform.io/daslearning-new/dl-test/google"
+  version = "0.0.3"
+  
+  bucket_name = "daslearning-m-bt"
+  location    = "asia-south1"
 }
 
 /*
-# Create bucket
-resource "google_storage_bucket" "bucket" {
-  name     = "daslearning-tf-cld"
-  location = "ASIA-SOUTH1"
-}
-
 resource "google_storage_bucket" "bucket2" {
   name     = "daslearning-tf-cld2"
   location = "ASIA-SOUTH1"
