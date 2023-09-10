d1b21f258340ec50b520aab88c97861437037f07
Somnath Das <somnath.das@daslearning.in>
2023-09-10 12:58:50 +0530

    delete resources

--- gcs.tf
+++ gcs.tf
@@ -1,19 +1,5 @@
 # Deploy gcs bucket using terraform cloud
 
-# Create bucket using module
-module "dl-test" {
-  source  = "app.terraform.io/daslearning-new/dl-test/google"
-  version = "0.0.4"
-  
-  bucket_name = "daslearning-m-bt"
-  location    = "asia-south1"
-  project     = "daslearning"
-}
-
-output "bucket_id" {
-  value = module.dl-test.bucket_id
-}
-
 /*
 provider "google" {
   project      = "daslearning"
@@ -22,5 +8,18 @@ provider "google" {
 resource "google_storage_bucket" "bucket2" {
   name     = "daslearning-tf-cld2"
   location = "ASIA-SOUTH1"
+}
+*/
+
+# Create bucket using module
+/*
+  
+  bucket_name = "daslearning-m-bt"
+  location    = "asia-south1"
+  project     = "daslearning"
+}
+
+output "bucket_id" {
+  value = module.dl-test.bucket_id
 }
 */
